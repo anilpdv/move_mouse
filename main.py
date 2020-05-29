9dd2524e21c1fc334e37c3aa8a77add1e6efe44d
anilpdv <pdvanil007@gmail.com>
2020-05-29 17:01:44 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+
+import pyautogui
+import threading
+import random
+import sys
+import signal
+
+
+# handles the signal
+def signal_handler(signal, frame):
+    print('ctrl + c')
+    sys.exit(0)
+
+
+# listening to the ctrl c event or any other keyboard interruptions
+signal.signal(signal.SIGINT, signal_handler)
+
+
+""" setinterval is functio that takes a function and no of seconds
+its recursive function creates a thread for every t seconds and call the function """
+
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+
+def moveMouse():
+    # taking width and height
+    width = pyautogui.size().width
+    height = pyautogui.size().height
+
+    # generating random width and height
+    randomWidth = random.randint(0, width)
+    randomHeight = random.randint(0, height)
+
+    # moving mouse to the particular position
+    pyautogui.moveTo(randomWidth, randomHeight, duration=1)
+
+
+# setting interval to call function evry t seconds
+t = set_interval(moveMouse, 40)
